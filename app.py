e275866f1f6ea131ccf3372b092259747e55d835
ksathvikreddy <ksathvikreddy31@gmail.com>
2026-02-17 22:16:58 +0530

    first

--- app.py
+++ app.py
@@ -24,5 +24,7 @@ def index():
 
     return render_template("index.html", result=result)
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
